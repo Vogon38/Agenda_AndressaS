f6360f87de7410a248791c97796a54e91b6985de
Vogon38 <luana@thecyborg.xyz>
2023-04-05 04:19:49 -0300

    adding all files

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useCallback } from 'react';
+
+// Default V2 theme
+import 'survey-core/defaultV2.min.css';
+// Modern theme
+// import 'survey-core/modern.min.css';
+import { Model } from 'survey-core';
+import { Survey } from 'survey-react-ui';
+import { surveyJson } from './json.js';
+
+// const SURVEY_ID = 1;
+
+function App() {
+  const survey = new Model(surveyJson);
+  const alertResults = useCallback((sender) => {
+    const results = JSON.stringify(sender.data);
+    alert(results);
+    // saveSurveyResults(
+    //   "https://your-web-service.com/" + SURVEY_ID,
+    //   sender.data
+    // )
+  }, []);
+
+  survey.onComplete.add(alertResults);
+
+  return <Survey model={survey} />;
+}
+
+// function saveSurveyResults(url, json) {
+//   const request = new XMLHttpRequest();
+//   request.open('POST', url);
+//   request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+//   request.addEventListener('load', () => {
+//     // Handle "load"
+//   });
+//   request.addEventListener('error', () => {
+//     // Handle "error"
+//   });
+//   request.send(JSON.stringify(json));
+// }
+
+export default App;
