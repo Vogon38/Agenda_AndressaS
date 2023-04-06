dedaac5c24a0292f81771702ecf430afce8e876f
Vogon38 <luana@thecyborg.xyz>
2023-04-06 00:32:45 -0300

    env file and firebase config fix

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ import { Survey } from 'survey-react-ui';
 import { surveyJson } from './json.js';
 import 'survey-core/defaultV2.min.css';
 
-require('dotenv').config();
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
